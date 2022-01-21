66f23c14fe7579c93c744bd7c9dd534c4cc274f1
AbHINe3T <57062938+AbHINe3T@users.noreply.github.com>
2022-01-21 10:57:33 +0530

    Initial commit

---
+++ sum.py
@@ -0,0 +1,26 @@
+#program that takes an input N  and displays the odd sum and even sum:
+n=int(input("Enter the Integer: \n"))
+sumeven=0
+sumodd=0
+while(n!=0):
+    digit=n%10
+    n=n//10
+    if(digit%2==0):
+     sumeven+=digit
+    else:
+     sumodd+=digit
+
+print("Sum of odd {}".format(sumodd))
+print("Sum of Even {}".format(sumeven))
+
+
+
+
+
+
+
+
+# n=int(input("enter the integer: \n"))
+# n=13
+# ans=mysum(n)
+# print(mysum)
